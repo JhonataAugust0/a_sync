cda40a787d6e4290ec571af4ead30bdc770a4d66
Jhonata Augusto <jhon.augustosilva@gmail.com>
2021-11-23 08:21:59 -0300

    Lista exibida a partir de requisição de api
    Aplicação flutter que consome a API http e os elementos de jsonplaceholder, utilizando esse para exibir os elementos na tela dentro de uma Column.
    Utilizou-se o padrão de projeto MVC.

---
+++ lib/controllers/posts_controller.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+import '../models/posts_model.dart';
+
+class PostsController {
+  ValueNotifier<List<Post>> posts = ValueNotifier<List<Post>>([]);
+  ValueNotifier<bool> inLoader = ValueNotifier<bool>(false);
+
+  callAPI() async {
+    var client = http.Client();
+    try {
+      inLoader.value = true;
+      var response = await client.get(
+        Uri.parse('https://jsonplaceholder.typicode.com/posts'),
+      );
+      var decodedResponse = jsonDecode(response.body) as List;
+      posts.value = decodedResponse.map((e) => Post.fromJson(e)).toList();
+    } finally {
+      client.close();
+      inLoader.value = false;
+    }
+  }
+}
---
+++ lib/views/one_page.dart
@@ -0,0 +1,47 @@
+import 'package:a_sync/widgets/custom_button_widget.dart';
+import 'package:flutter/material.dart';
+
+import '../controllers/posts_controller.dart';
+
+class OnePage extends StatefulWidget {
+  const OnePage({Key? key}) : super(key: key);
+
+  @override
+  State<OnePage> createState() => _OnePageState();
+}
+
+class _OnePageState extends State<OnePage> {
+  final PostsController _controller = PostsController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+          child: SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            AnimatedBuilder(
+                animation:
+                    Listenable.merge([_controller.posts, _controller.inLoader]),
+                builder: (_, __) => _controller.inLoader.value
+                    ? CircularProgressIndicator()
+                    : ListView.builder(
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: _controller.posts.value.length,
+                        itemBuilder: (_, idx) => ListTile(
+                            title: Text(_controller.posts.value[idx].title)),
+                      )),
+            SizedBox(height: 12),
+            CustomButtonWidget(
+                disable: false,
+                titleSize: 18,
+                onPressed: () => _controller.callAPI(),
+                title: 'Custom button'),
+          ],
+        ),
+      )),
+    );
+  }
+}
